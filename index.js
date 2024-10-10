007ae871413978b2a9eae0eae6451835e61825b7
sanji118 <sanjishifa32@gmail.com>
2024-10-10 13:09:20 +0600

    added header section

---
+++ index.js
@@ -0,0 +1,11 @@
+//card html
+/**<div class="flex gap-5">
+                <div>
+                    <img src="" alt="">
+                </div>
+                <div>
+                    <h4 class="font-semibold text-2xl"></h4>
+                    <p></p>
+                    <button class="font-bold text-[#FFC107]">View Details</button>
+                </div>
+            </div> */
