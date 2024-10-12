c92a2c728199319a3ae50574360241b847f4fdde
sanji118 <sanjishifa32@gmail.com>
2024-10-12 21:21:11 +0600

    added category wise cards

--- fetch.js
+++ fetch.js
@@ -5,7 +5,7 @@ const loadCategories = async () =>{
     displayAllCategories(data.categories);
 }
 loadCategories();
-
+//see more see less functionality
 const toggleText = (event, id)=>{
     event.preventDefault();
     const dots = document.getElementById(`dots-${id}`);
@@ -21,3 +21,57 @@ const toggleText = (event, id)=>{
         btnText.innerText ="See Less";
     }
 }
+//details
+const getDetails = async (mealId) => {
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`
+    );
+    const data = await response.json();
+    //displayDetails(data.meals);
+};
+
+//getDetails();
+const getFoodsById = async () =>{
+    const res = await fetch('https://www.themealdb.com/api/json/v1/1/list.php?c=list');
+    const data = await res.json();
+    //displayAllFoods(data.meals);
+}
+//getFoodsById();
+
+
+const getFoodsByFirstLetter = async (firstLetter) => {
+    const res = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?f=${firstLetter}`
+    );
+    const data = await res.json();
+    if (data.meals === null) {
+      return "No data";
+    }
+  
+    displayAllFoods(data.meals);
+};
+
+
+const getFoodsByCategory = async (category) => {
+    const res = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category}`
+    );
+    const data = await res.json();
+    if (data.meals === null) {
+      return "No data";
+    }
+  
+    displayAllFoods(data.meals);
+};
+
+//latest food
+const getLatestMeal = async () => {
+  const res = await fetch('https://www.themealdb.com/api/json/v1/1/latest.php');
+  const data = await res.json();
+  if (data.meals === null) {
+    return "No data";
+  }
+
+  displayLatestMeals(data.meals);
+};
+getLatestMeal();
--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const displayAllCategories = (categories) =>{
         const categoryContainer = document.getElementById('categoryContainer');
         const categoryCard = document.createElement('div');
         categoryCard.innerHTML=`
-        <div class="card md:card-side border-base-200 border">
+        <div class="card md:card-side border-base-200 border category-btn">
             <figure class="w-60">
                 <img
                 src=${category.strCategoryThumb}
@@ -34,5 +34,43 @@ const displayAllCategories = (categories) =>{
         categoryContainer.classList = 'carousel rounded-box m-5 gap-5 w-full';
         categoryCard.classList ='carousel-item';
         categoryContainer.appendChild(categoryCard);
+        categoryCard.onclick = () =>{
+            removeActiveClass();
+            categoryCard.classList.add('bg-base-200', 'rounded-lg');
+            getFoodsByCategory(category.strCategory);
+        }
+    })
+}
+const removeActiveClass = () =>{
+    const categoryBtn = document.getElementsByClassName('category-btn');
+    for(btn of categoryBtn) {
+      btn.classList.remove('bg-base-200');
+    };
+}
+//display foods by choosing category
+const displayAllFoods = (meals) =>{
+    const foodContainer = document.getElementById('displayAllFoods');
+    foodContainer.innerHTML = '';
+    meals.forEach(meal =>{
+        
+        const foodCard = document.createElement('div');
+        const title = meal.strMeal;
+        foodCard.innerHTML = `
+            <figure>
+                <img src="${meal.strMealThumb}" class="w-4/5"/>
+            </figure>
+            <div class="py-2 text-center w-3/4"><p  class="title font-bold text-lg">${title}</p></div>
+            
+        `;
+        foodContainer.classList = 'grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-7 p-8'
+        foodContainer.appendChild(foodCard);
+        //getFoodsByCategory('beef');
+    })
+}
+
+//display latest meals
+const displayLatestMeals = (meals) =>{
+    meals.forEach(meal =>{
+        console.log(meal);
     })
 }
